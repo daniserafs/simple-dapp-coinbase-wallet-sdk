8a2ba2774d2a2c0c8db9b4976a75c830f2c7297b
Daniela Serafim Soares <danyelaserafim@gmail.com>
2022-03-28 19:16:01 -0300

    polyfill fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import NodeGlobalPolyfillPlugin from "@esbuild-plugins/node-globals-polyfill";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [react()],
   resolve: {
     alias: {
